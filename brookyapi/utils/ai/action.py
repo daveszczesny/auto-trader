0ba596c81952c5b3592baea996c08be5188e1c76
Dave Szczesny <dave.szczesny@genesys.com>
2024-11-03 18:03:34 +0000

    Refactored API code

--- brookyapi/utils/ai/action.py
+++ brookyapi/utils/ai/action.py
@@ -1,14 +1,19 @@
+import logging
+from enum import Enum
+from typing import Tuple, Optional, Dict
 import torch
 import numpy as np
-from enum import Enum
+
+from utils.ai.exceptions import ErrorSet, StatusCode
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger('BrookyAPI')
 
 class ActionType(Enum):
     DO_NOTHING = 0
     LONG = 1
     SHORT = 2
     CLOSE = 3
-
-
 
 action_type_mapping = {
     0: ActionType.DO_NOTHING,
@@ -22,16 +27,20 @@ def get_action_type(action: torch.Tensor) -> ActionType:
     return action_type_mapping.get(index, ActionType.DO_NOTHING)
 
 
-def construct_action(action: np.ndarray) -> ActionType:
-    action_type = get_action_type(action)
+def construct_action(action: np.ndarray) -> Tuple[Dict[str, str], Optional[ErrorSet], int]:
+    try:
+        action_type = get_action_type(action)
 
-    response = {'action': action_type.name}
+        response = {'action': action_type.name}
 
-    if action_type in [ActionType.LONG, ActionType.SHORT]:
-        response.update({
-            'lot_size': action[1].item(),
-            'stop_loss': None,
-            'take_profit': None
-        })
+        if action_type in [ActionType.LONG, ActionType.SHORT]:
+            response.update({
+                'lot_size': action[1].item(),
+                'stop_loss': None,
+                'take_profit': None
+            })
+        return response, None, StatusCode.ACCEPTED
 
-    return response
+    except Exception as ex:
+        logger.error('Error while constructing action', exc_info=ex)
+        return {}, ErrorSet.UNKNOWN_ERROR, StatusCode.INTERNAL_SERVER_ERROR
--- brookyapi/utils/ai/observation.py
+++ brookyapi/utils/ai/observation.py
@@ -1,12 +1,29 @@
 import logging
-from typing import Optional, Tuple
+from typing import Optional, Tuple, Dict, Any
 
 import numpy as np
+
+from utils.ai.exceptions import ErrorSet, StatusCode
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger("BrookyAPI")
 
-def construct_observation(payload) -> Tuple[np.ndarray, Optional[str]]:
+def construct_observation(payload: Dict[str, Any]) -> Tuple[Optional[np.ndarray], Optional[ErrorSet], int]:
+    """
+    Construct an observation from the payload.
+
+    :param payload: 
+        The input data containing 
+            - balance, 
+            - unrealized_pnl,
+            - current_price,
+            - current_high,
+            - current_low,
+            - open_trades,
+            - indicators
+        :return: A tuple containing the observation array and 
+            an error message if any required fields are missing.
+    """
     balance = payload.get('balance', None)
     unrealized_pnl = payload.get('unrealized_pnl', None)
     current_price = payload.get('current_price', None)
@@ -22,9 +39,9 @@ def construct_observation(payload) -> Tuple[np.ndarray, Optional[str]]:
                                        current_low,
                                        open_trades,
                                        indicators)):
-        logger.error(f'Missing required fields: {repr(payload)}')
-        return None, 'Missing required fields'
-    
+        logger.error(f'Required fields not found in payload: {repr(payload)}')
+        return None, ErrorSet.INVALID_INPUT, StatusCode.BAD_REQUEST
+
     # retrieve expected indicators from payload
     indicator_values = {indicator['name']: indicator['value'] for indicator in indicators}
     ema_200 = indicator_values.get('ema_200', None)
@@ -32,9 +49,8 @@ def construct_observation(payload) -> Tuple[np.ndarray, Optional[str]]:
     ema_21 = indicator_values.get('ema_21', None)
 
     if any(value is None for value in (ema_200, ema_50, ema_21)):
-        logger.error(f'Missing expected indicators: {repr(indicator_values)}')
-        return None, 'Missing expected indicators'
-    
+        logger.error(f'Expected indicators not found in payload: {repr(indicator_values)}')
+        return None, ErrorSet.INVALID_INPUT, StatusCode.BAD_REQUEST
 
     # construct observation
     observation = np.array(
@@ -54,6 +70,6 @@ def construct_observation(payload) -> Tuple[np.ndarray, Optional[str]]:
 
     if len(observation) != 9:
         logger.error(f'Observation does not contain 9 elements: {repr(observation)}')
-        return 'Observation does not contain 9 elements'
-    
-    return observation, None
+        return ErrorSet.INVALID_OBSERVATION_LENGTH, StatusCode.BAD_REQUEST
+
+    return observation, None, StatusCode.OK
